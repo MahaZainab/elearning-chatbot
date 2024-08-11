3ca44b2ba9bb607908bf249b55729395263ace96
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-08-12 00:11:57 +0500

    chatbot detail

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
 
-st.title("Coursera  Q&A 🧑‍🏫")
+st.title("Udemy  Q&A 🧑‍🏫")
 #btn = st.button("Create Knowledgebase")
 #if btn:
 #    create_vector_db()
